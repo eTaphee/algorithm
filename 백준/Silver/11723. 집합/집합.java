07e4b1ed61503eb5ecdce53c9c4b6243e14db66d
eTaphee <43163409+eTaphee@users.noreply.github.com>
2024-03-22 15:05:30 +0900

    [Silver V] Title: 집합, Time: 1192 ms, Memory: 313436 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -4,17 +4,11 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class Main {
 
   static int M;
   static Set<Integer> set = new HashSet<>();
-  static Set<String> noValueOp = new HashSet<>() {{
-    add("all");
-    add("empty");
-  }};
-
   static StringBuilder sb = new StringBuilder();
 
   public static void main(String[] args) throws IOException {
@@ -23,15 +17,12 @@ public class Main {
     for (int i = 0; i < M; i++) {
       StringTokenizer tokenizer = new StringTokenizer(br.readLine());
       String op = tokenizer.nextToken();
-      if (noValueOp.contains(op)) {
-        switch (op) {
-          case "all":
-            IntStream.range(1, 21).forEach(m -> set.add(m));
-            break;
-          case "empty":
-            set.clear();
-            break;
+      if ("all".equals(op)) {
+        for (int n = 1; n <= 20; n++) {
+          set.add(n);
         }
+      } else if ("empty".equals(op)) {
+        set.clear();
       } else {
         int value = Integer.parseInt(tokenizer.nextToken());
         switch (op) {
