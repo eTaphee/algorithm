08496f17fe714efc8bd9f40e0b5c02b60951bf45
eTaphee <43163409+eTaphee@users.noreply.github.com>
2024-03-27 19:49:21 +0900

    [Silver IV] Title: 등수 구하기, Time: 84 ms, Memory: 11700 KB -BaekjoonHub

--- 백준/Silver/1205. 등수 구하기/등수 구하기.java
+++ 백준/Silver/1205. 등수 구하기/등수 구하기.java
@@ -1,19 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
-  static int N;
-  static int score;
-  static int P;
-  static int rank;
-  static List<Integer> rankList = new ArrayList<>();
+  static Integer N, score, P;
 
+  static Integer[] ranking;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,37 +18,45 @@ public class Main {
     N = Integer.parseInt(tokenizer.nextToken());
     score = Integer.parseInt(tokenizer.nextToken());
     P = Integer.parseInt(tokenizer.nextToken());
+    ranking = new Integer[P];
 
-    if (N > 0) {
-      tokenizer = new StringTokenizer(br.readLine());
-      for (int i = 0; i < N; i++) {
-        rankList.add(Integer.parseInt(tokenizer.nextToken()));
+    if (N == 0) {
+      System.out.println(1);
+      System.exit(0);
+    }
+
+    tokenizer = new StringTokenizer(br.readLine());
+    for (int i = 0; i < P; i++) {
+      if (tokenizer.hasMoreTokens()) {
+        ranking[i] = Integer.parseInt(tokenizer.nextToken());
+      } else {
+        ranking[i] = -1;
       }
     }
 
-    rankList.sort(Comparator.reverseOrder());
+    // 점수 내림차순 정렬
+    Arrays.sort(ranking, Comparator.reverseOrder());
 
-    // 랭크 리스트에 올라갈 수 없을 정도로 낮은 경우 -1
-    if (!rankList.isEmpty()) {
-      int last = rankList.get(rankList.size() - 1);
-      if (N == P && score <= last) {
-        System.out.println(-1);
-        System.exit(0);
-      }
-    }
+    int rank = -1;
+    for (int i = 0; i < P; i++) {
+      Integer cur = ranking[i];
 
-    rank = 1;
-    for (int current : rankList) {
-      if (current >= score) {
-        rank++;
-        if (current == score) {
-          rank--;
+      int comp = cur.compareTo(score);
+      if (comp <= 0) {
+        if (i == P - 1 && comp == 0) { // 위치가 마지막이고 점수가 같으면 들어갈 수 없음
           break;
         }
-      } else {
+
+        if (score.compareTo(ranking[P - 1]) == 0) {
+          break;
+        }
+
+        rank = i + 1;
         break;
       }
     }
+
     System.out.println(rank);
   }
 }
+
