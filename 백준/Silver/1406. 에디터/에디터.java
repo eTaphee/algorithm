bff272c7445fc42fa0cd13eb749c9f0ba1f72160
eTaphee <43163409+eTaphee@users.noreply.github.com>
2024-04-04 18:49:23 +0900

    [Silver II] Title: 에디터, Time: 636 ms, Memory: 115080 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
@@ -44,8 +46,11 @@ public class Main {
       }
     }
 
-    StringBuilder sb = new StringBuilder();
-    list.forEach(sb::append);
-    System.out.println(sb);
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    for (Character character : list) {
+      bw.write(character);
+    }
+    bw.flush();
+    bw.close();
   }
 }
